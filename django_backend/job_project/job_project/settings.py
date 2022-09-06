da24ed10890422e59f7f09105677621c96b2f476
drishtantR1508 <drishtant.rai.min19@itbhu.ac.in>
2022-09-06 06:52:34 +0000

    ...

--- django_backend/job_project/job_project/settings.py
+++ django_backend/job_project/job_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-oap-80u@%my2gbkji!5^jscw1w5eu9y5z_z&xonrao+egkpk!5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
