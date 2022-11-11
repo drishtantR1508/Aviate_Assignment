53c8fb49843c06f59e427ff062ba765b8362804e
drishtantR1508 <drishtant.rai.min19@itbhu.ac.in>
2022-11-11 20:35:58 +0530

    ...

--- django_backend/job_project/job_app/api.py
+++ django_backend/job_project/job_app/api.py
@@ -1,10 +1,65 @@
 from rest_framework import viewsets,permissions
+from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
 from .serializers import ApplicationSerializer
 from .models import Application
 from django.core.files import File
 from job_project.settings import BASE_DIR, MEDIA_ROOT
+from django.http.response import Http404
+from rest_framework.response import Response
+
+class ApplicationAPIView(APIView):
+    
+    def get_applicant(self,pk):
+        return Application.objects.get(pk=pk)
+        
+
+    def get(self,request,pk=None,format=None):
+        if pk:
+            data = self.get_applicant(pk=pk)
+            serializer = ApplicationSerializer(data)
+            return Response(serializer.data)
+        else:
+            data = Application.objects.all()
+            serializer = ApplicationSerializer(data, many=True)
+            return Response(serializer.data)
+        
+    
+
+    def post(self,request,format=None):
+        serializer = ApplicationSerializer(data=request.data,partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        response = Response()
+        response.data = {
+            'message': 'Applicant Created Successfully',
+            'data': serializer.data
+        }
+        return response
+        
+
+
+
+    def put(self,request,pk,format=None):
+        applicant = Application.objects.get(pk=pk)
+        serializer = ApplicationSerializer(instance = applicant,data=request.data,partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        response = Response()
+        response.data = {
+            'message': 'Applicant updated Successfully',
+            'data': serializer.data
+        }
+        return response
+
+
+
+    def delete(self, request,pk,format=None):
+        Application.objects.get(pk=pk).delete()
+        return Response({
+            'message': 'Deleted Successfully'
+        })
 
 class ApplicationViewset(viewsets.ModelViewSet):
     permission_classes = [
--- django_backend/job_project/job_app/urls.py
+++ django_backend/job_project/job_app/urls.py
@@ -1,7 +1,12 @@
 from rest_framework import routers
-from .api import ApplicationViewset
+from .api import ApplicationViewset,ApplicationAPIView
 from django.urls import path
-router = routers.DefaultRouter()
-router.register('api/applications', ApplicationViewset, 'applications')
+# router = routers.DefaultRouter()
+# router.register('api/applications', ApplicationViewset, 'applications')
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
+urlpatterns = [
+    path('api/applications/<int:pk>',ApplicationAPIView.as_view()),
+    path('api/applications/',ApplicationAPIView.as_view()),
+    
+]
