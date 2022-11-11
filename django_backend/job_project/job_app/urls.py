d718ca7b769559eb182f02cafdcd966c339c32ff
drishtantR1508 <drishtant.rai.min19@itbhu.ac.in>
2022-11-11 20:38:40 +0530

    ...

--- django_backend/job_project/job_app/urls.py
+++ django_backend/job_project/job_app/urls.py
@@ -1,12 +1,12 @@
 from rest_framework import routers
 from .api import ApplicationViewset,ApplicationAPIView
 from django.urls import path
-# router = routers.DefaultRouter()
-# router.register('api/applications', ApplicationViewset, 'applications')
+router = routers.DefaultRouter()
+router.register('viewsets/api/applications/', ApplicationViewset, 'applications')
 
-# urlpatterns = router.urls
 urlpatterns = [
     path('api/applications/<int:pk>',ApplicationAPIView.as_view()),
     path('api/applications/',ApplicationAPIView.as_view()),
     
 ]
+urlpatterns += router.urls
